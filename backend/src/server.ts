0d91e7c2c0e524381ee771ee7bb272edc04a528d
Diego Nascimento <nascimentodln22@gmail.com>
2026-09-10 15:07:20 -0300

    chore: set up backend

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import  express  from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({message: "API is running"})
+})
+
+app.listen(3000, () => { console.log("Server running on port 3000");
+ });
