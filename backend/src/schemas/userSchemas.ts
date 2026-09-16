f21924389938b86ca122ecd410b60d2c02659562
Diego Nascimento <nascimentodln22@gmail.com>
2026-09-16 15:53:34 -0300

    feat: add user validation schema

---
+++ backend/src/schemas/userSchemas.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const createUserSchema = z.object({
+    name: z.string().min(2),
+    email: z.string(),
+    password: z.string().min(8)
+})
+
+export type createUserDTO = z.infer<typeof createUserSchema>;
