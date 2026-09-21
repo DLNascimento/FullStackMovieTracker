aee83cb9bb91b1f160b04ccf238c271facd1cf03
Diego Nascimento <nascimentodln22@gmail.com>
2026-09-21 10:59:00 -0300

    feat: add user request validation

--- backend/src/schemas/userSchemas.ts
+++ backend/src/schemas/userSchemas.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createUserSchema = z.object({
     name: z.string().min(2),
-    email: z.string(),
+    email: z.email(),
     password: z.string().min(8)
 })
 
